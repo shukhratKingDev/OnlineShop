8dc012caeaf2985847d5353e7d2b020c4c77c2b7
shukhratKingDev <shukhratDev1201@gmail.com>
2023-02-11 22:44:46 +0500

    first commit

---
+++ src/main/java/uz/company/asaxiy/AsaxiyApplication.java
@@ -0,0 +1,13 @@
+package uz.company.asaxiy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsaxiyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsaxiyApplication.class, args);
+    }
+
+}
